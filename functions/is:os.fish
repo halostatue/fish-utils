9643563ed71fa19326374d01d47940ed1e22ccb0
Austin Ziegler <austin@zieglers.ca>
2019-06-17 16:40:43 -0400

    v1.1.1: Improve is:os implementation

--- functions/is:os.fish
+++ functions/is:os.fish
@@ -1,7 +1,6 @@
 function is:os -a type -d 'Returns true if the current OS is the specified type'
-    if test (string length $type) -eq 0
-        return 1
-    end
+    test -z $type
+    and return 1
 
     string match -qe $type (string lower (uname -s))
 end
